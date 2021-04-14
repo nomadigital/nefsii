2c7307a14573b3e0f61b29ceab83f06a3b802955
nomadigital <n-d@tuta.io>
2021-04-14 19:52:38 +0200

    update pwa

---
+++ docs/_nuxt/59f5eca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{274:function(t,e,n){"use strict";n.r(e);var l=n(67),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("img",{attrs:{height:"180",width:"180",alt:"Logo",src:"/nefsii/logo.svg"}})}),[],!1,null,null,null);e.default=component.exports}}]);
